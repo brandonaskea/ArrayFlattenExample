4571045b7eaacc3217c6d41d66550b19a8537533
Brandon Askea <brandon.askea@me.com>
2019-02-07 14:23:35 -0800

    Initial Commit

---
+++ ArrayFlattenExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArrayFlattenExample
+//
+//  Created by Brandon Askea on 2/7/19.
+//  Copyright © 2019 Brandon Askea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
